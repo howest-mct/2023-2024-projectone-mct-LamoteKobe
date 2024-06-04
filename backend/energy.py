c3802280ac86f61655f208ca60c96ee63c2949e9
Kobe Lamote <kobe.lamote@student.howest.be>
2024-06-04 12:03:51 +0200

    wh berekenen

--- backend/energy.py
+++ backend/energy.py
@@ -1,22 +1,28 @@
 import time
 import RPi.GPIO as GPIO
 
-
+lasttime = time.monotonic()
 
 def pulse(pin):
-    start = time.monotonic()
+    starttime = time.monotonic()
+    global lasttime
     while GPIO.input(pin):
-        if time.monotonic() - start > 0.01:
-            print("pulse")
+        if time.monotonic() - starttime > 0.01:
+            # print("pulse")
+            tijd = time.monotonic() - lasttime
+            wH = 3600 / tijd
+            print(f"tijd {tijd}")
+            print(f"wH: {wH}wH")
+            lasttime = time.monotonic()
             return
-    print("FILTER")
+    # print("FILTER")
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(18, GPIO.RISING, callback=pulse, bouncetime=80)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(13, GPIO.RISING, callback=pulse, bouncetime=80)
 
-GPIO.output(5, 1)
+GPIO.output(19, 1)
 
 try:
     while True:
